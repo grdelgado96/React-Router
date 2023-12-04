befcc0aee8c7e50b1759286cb2891ef064a282cf
Giselle <rodriguezdelgadogiselle@gmail.com>
2023-12-04 12:05:15 -0500

    routes ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  // createRoutesFromElements,
+  // Route,
+} from "react-router-dom";
+import HomePage from "./pages/Home";
+import ProductPage from "./pages/Products";
+import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
+import ProductDetailPage from "./pages/ProductDetail";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout></RootLayout>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      { index:true, element: <HomePage></HomePage> },
+      { path: "products", element: <ProductPage /> },
+      { path: "products/:productId", element: <ProductDetailPage /> },
+    ],
+  },
+]);
+
+// const routeDefinitions= createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage></HomePage>}></Route>
+//     <Route path="/products" element={<ProductPage></ProductPage>}></Route>
+//   </Route>
+// );
+// const router = createBrowserRouter(routeDefinitions);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
---
+++ src/pages/Error.js
@@ -0,0 +1,13 @@
+import MainNavigation from "../components/MainNavigation";
+
+function ErrorPage(){
+    return <>
+    <MainNavigation></MainNavigation>
+    <main >
+        <h1>An error ocurred!</h1>
+        <p>Could not find this page!</p>
+    </main>
+    </>
+}
+
+export default ErrorPage;
